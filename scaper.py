80a8054aa7d5062a593aedeffa3a96691b6dbc3f
beanbeanchris <christophercastroramirez@gmail.com>
2023-12-20 08:35:03 -0800

    code can now login and get the private FB group.

--- scaper.py
+++ scaper.py
@@ -12,24 +12,42 @@ import datetime
 from bs4 import BeautifulSoup
 import pandas as pd 
 from dotenv import load_dotenv
+load_dotenv()
 import os
+from selenium.webdriver.chrome.service import Service
+
+
+
 EMAIL = os.getenv("MY_EMAIL")
 PASSWORD = os.getenv("MY_PASSWORD")
+FB_GROUP = os.getenv("FB_GROUP_SITE")
 chrome_options = Options()
 chrome_options.add_argument("--disable-notifications")
 
-# Create an empty DataFrame with these columns
+# # Create an empty DataFrame with these columns
 columns = ['date_posted', 'location', 'land_area', 'price']
 df = pd.DataFrame(columns=columns)
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)  
+service = Service()
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
 driver.get("https://www.facebook.com")
 driver.maximize_window()
 sleep(2)
 
-cookies = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="_42ft _4jy0 _9o-t _4jy3 _4jy1 selected _51sy"]'))).click()
-email = driver.find_element_by_id("email")
+email = driver.find_element(By.ID, "email")
 email.send_keys(EMAIL)
+sleep(1)
+print(EMAIL)
+password = driver.find_element(By.NAME, "pass")
+password.send_keys(PASSWORD)
+sleep(1)
+print(PASSWORD)
+login = driver.find_element(By.NAME, "login")
+login.click()
+sleep(1)
+driver.get(FB_GROUP) # change group here
+sleep(4)
 
 
 
